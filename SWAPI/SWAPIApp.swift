982145473089f7d60a3a42cdb5cb130c0a02e7a6
Stephen Byatt <47413006+stevebyatt10@users.noreply.github.com>
2021-10-05 14:37:10 +1100

    Initial Commit

---
+++ SWAPI/SWAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPIApp.swift
+//  SWAPI
+//
+//  Created by Stephen Byatt on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SWAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
